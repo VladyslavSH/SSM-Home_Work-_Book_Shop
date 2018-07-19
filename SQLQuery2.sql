728de2bf69d88341c72816a7f9054467553f8476
VladyslavSH <manujlenkovlad2@gmail.com>
2018-07-19 16:37:45 +0300

    Work is changed

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -14,6 +14,11 @@ insert into Sales values
 ( 11 , (select NameBook from books where id_book = 11 ), '2018-07-10',(select price from books where id_book = 11), 1 ,6),
 ( 11 , (select NameBook from books where id_book = 11 ), '2018-07-10',(select price from books where id_book = 11), 1 ,6)
 go
+
+insert into Books values
+	( 'Супер Программирование',6, 14 , 1000.20,	'no pic',  '2018-02-05', 500 ),
+	( 'Супер Программирование 2',6, 14 , 780.20,	'no pic',  '2018-02-05', 250 )
+
 ------------ LessOn 1 --------------
 create view Autors_From (A_FirstName, A_LastName, NameCountry)
 as
@@ -63,11 +68,25 @@ go
 ------------ CoMpleCt --------------
 
 ------------ LessOn 4 --------------
-select * from Sales
+create view TopBook
+as
+select top 1 books.NameBook,  count(Sales.Price*Sales.Quantity) as 'count'  from books, Sales where books.ID_BOOK = Sales.ID_BOOK  group by books.NameBook order by 2 desc
+
+select * from TopBook
+go
+------------ CoMpleCt --------------
+
+------------ LessOn 5 --------------
+create view AuthorsAB
+as
+select * from Authors where Authors.FirstName like '[АБ]%'
 go
 
-select Books.NameBook, count(Books.ID_BOOK) from Books, Sales
-where Books.ID_BOOK = sales.ID_BOOK
-group by Books.NameBook
-having count(Books.ID_BOOK)>all (select count(Books.ID_BOOK) from Books)
+select * from AuthorsAB
 go
+------------ CoMpleCt --------------
+
+------------ LessOn 6 --------------
+select Shops.NameShop from Shops
+where
+Shops.ID_SHOP <> any (select Sales.ID_SHOP from Sales)
